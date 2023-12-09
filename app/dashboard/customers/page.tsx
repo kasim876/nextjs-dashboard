d948dda86f0c06a9176d924540bfe391d6687426
Maxim Lukashenko <max.lukashenko2017@gmail.com>
2023-12-10 00:13:21 +0300

    add customers table

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,38 @@
 import {Metadata} from 'next';
+import Table from '@/app/ui/customers/table';
+import {lusitana} from '@/app/ui/fonts';
+import {fetchCustomersPages} from '@/app/lib/data';
+import Search from '@/app/ui/search';
+import Pagination from '@/app/ui/pagination';
 
 export const metadata: Metadata = {
   title: 'Customers',
 };
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page || 1);
+
+  const totalPages = await fetchCustomersPages(query);
+
+  return (
+    <div className="w-full">
+      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>Customers</h1>
+      <Search placeholder="Search customers..." />
+      <Table
+        currentPage={currentPage}
+        query={query}
+      />
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+    </div>
+  );
 }
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -166,6 +166,25 @@ export async function fetchInvoiceById(id: string) {
   }
 }
 
+export async function fetchCustomersPages(query: string) {
+  noStore();
+
+  try {
+    const count = await sql`SELECT COUNT(*)
+      FROM customers
+      WHERE
+        customers.name ILIKE ${`%${query}%`} OR
+        customers.email ILIKE ${`%${query}%`}
+    `;
+
+    const totalPages = Math.ceil(Number(count.rows[0].count) / ITEMS_PER_PAGE);
+    return totalPages;
+  } catch (error) {
+    console.error('Database error:', error);
+    throw new Error('Failed to fetch total number of customers.');
+  }
+}
+
 export async function fetchCustomers() {
   noStore();
 
@@ -186,8 +205,9 @@ export async function fetchCustomers() {
   }
 }
 
-export async function fetchFilteredCustomers(query: string) {
+export async function fetchFilteredCustomers(query: string, currentPage: number) {
   noStore();
+  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
     const data = await sql<CustomersTable>`
@@ -205,7 +225,8 @@ export async function fetchFilteredCustomers(query: string) {
 		  customers.name ILIKE ${`%${query}%`} OR
         customers.email ILIKE ${`%${query}%`}
 		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
+		ORDER BY customers.name DESC
+    LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
 	  `;
 
     const customers = data.rows.map(customer => ({
